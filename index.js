5897d1742cff9354abb83037e5b02301b5d6b615
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-18 12:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var elem = document.querySelector('div#grand-node')
-  let current = array
+  let current = []
   let next = []
   
   while (current) {
